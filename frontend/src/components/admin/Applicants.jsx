dba512c437e25707a5797f6534426739bb201a9a
Vishal Kumar <iamvishalumar30@gmail.com>
2024-11-26 22:37:11 +0530

    updated url

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -14,6 +14,7 @@ const Applicants = () => {
 
     useEffect(() => {
         const fetchAllApplicants = async () => {
+           
             try {
                 const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, { withCredentials: true });
                 dispatch(setAllApplicants(res.data.job));
--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
         e.preventDefault();
         try {
             dispatch(setLoading(true));
-            const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+            const res = await axios.post("https://job-portal-website-mern.onrender.com/api/v1/user/login", input, {
                 headers: {
                     "Content-Type": "application/json"
                 },
